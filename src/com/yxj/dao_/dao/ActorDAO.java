85bc84d2ae1d94163193637adcf508fc233fed33
yuanxiujie <3177873998@qq.com>
2022-08-04 01:11:15 +0800

    secend commit

---
+++ src/com/yxj/dao_/dao/ActorDAO.java
@@ -0,0 +1,11 @@
+package com.yxj.dao_.dao;
+
+import com.yxj.dao_.domain.Actor;
+
+/**
+ * @Author:zhili
+ * @Time:2022/8/4 1:08
+ * @describe:
+ */
+public class ActorDAO extends BasicDAO<Actor>{
+}
--- src/com/yxj/dao_/domain/Actor.java
+++ src/com/yxj/dao_/domain/Actor.java
@@ -6,4 +6,38 @@ package com.yxj.dao_.domain;
  * @describe:
  */
 public class Actor {
+    private String name;
+    private Integer money;
+
+    public Actor() {
+    }
+
+    public Actor(String name, Integer money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getMoney() {
+        return money;
+    }
+
+    public void setMoney(Integer money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "name='" + name + '\'' +
+                ", money=" + money +
+                '}';
+    }
 }
